87834a6d1b5e2f4b4c1e1a95c1c3d53a3cc11821
labormx <labormx@greenhouse9.com>
2018-05-01 21:58:32 +0900

    table ddl 作成

--- db/ddl/create_table.sql
+++ db/ddl/create_table.sql
@@ -2,5 +2,23 @@
 CREATE TABLE m_exam
 (
 	id int NOT NULL,
-	name varchar(100),
+	name varchar(100)
+);
+-- 試験問題マスタ
+CREATE TABLE m_exam_problem_header
+(
+    id int NOT NULL,
+    exam_id int NOT NULL,
+    description varchar(100),
+    year year,
+    number tinyint
+);
+-- 試験問題詳細マスタ
+CREATE TABLE m_exam_problem_detail
+(
+    id int NOT NULL,
+    exam_problem_id int NOT NULL,
+    part char(1),
+    p_number tinyint,
+    problem blob
 );
